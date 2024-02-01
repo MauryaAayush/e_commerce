6f8a635ffefb9a2e976be4a6b4d260596777517b
MauryaAayush <maayush235@gmail.com>
2024-02-01 23:38:27 +0530

    Radhey radhey

--- lib/main.dart
+++ lib/main.dart
@@ -14,16 +14,59 @@ class FirstPage extends StatelessWidget {
       debugShowCheckedModeBanner: false,
 
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.grey,
-          title:  Text("My app"),
-          centerTitle: true,
-        ),
+
         backgroundColor: Colors.blue,
 
-        body: Column(
+        body: Center(
+          // main container
+          child: Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF6951FF),
+                  Color(0xFF8C50FE),
+                  Color(0xFF9844F9),
+                  Colors.purple,
+                ],
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+              )
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                     Row(
+                      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          height: 25,
+                          width: 170,
+                          decoration: BoxDecoration(
+                             color: Colors.white,
+                            borderRadius: BorderRadius.all(Radius.circular(50),),
+                          ),
+                          child: Text('Search',style: TextStyle(
+                            color: Colors.blue,
+                          ),
 
-        ),
+                          ),
+
+                          margin: EdgeInsets.symmetric(horizontal: 15,vertical: 30),
+                          padding: EdgeInsets.symmetric(horizontal:10),
+                        )
+                      ],
+                    ),
+
+                ],
+              ),
+          )
+          ),
+        )
       ),
     );
   }
